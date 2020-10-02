6b8b95da7483ac91c97957ba6a8a182a27eec19c
Jason Thomas <jasonthomasjat@gmail.com>
2020-10-02 16:07:55 -0700

    updated to check password and display secret

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+
 const loginForm = document.createElement("form");
 const nameLabel = document.createElement("label");
 const userName = document.createElement("input");
 const pwLabel = document.createElement("label");
 const password = document.createElement("input");
 const loginButton = document.createElement("button");
+loginButton.setAttribute("type", "submit");
+
 const secretMSG = document.createElement("h1");
-let users;
+let authorizedUsers = [];
 
 document.body.appendChild(loginForm);
 loginForm.appendChild(nameLabel);
@@ -24,25 +27,28 @@ loginButton.innerText = "Log in";
 let xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
   if(this.readyState == 4 && this.status == 200) {
-    users = (JSON.parse(xhttp.responseText));
-  } 
+   let users = (JSON.parse(xhttp.responseText).users);
+    for(user of users){
+      authorizedUsers.push(user);
+    }         
+  }
+ 
 };
 xhttp.open("GET", "users.json", true);
 xhttp.send();
 
-loginButton.addEventListener('click', () =>{  
-  checkAuthenticity(userName.innerText, password.innerText);
-  console.log(userName.value, password.value);
+loginButton.addEventListener('click', (event) =>{  
+  event.preventDefault();      
+  checkAuthenticity(userName.value, password.value); 
 });
 
 
+
 function checkAuthenticity(userName, password){
-  
-  users.forEach(user => {
-    console.log(user);
+  console.log("checkAuthenticity");
+  authorizedUsers.forEach(user => {    
     if(user.name === userName && user.password === password){
       secretMSG.textContent = user.secret;      
-      console.log(secretMSG);
     }
   });
 }
